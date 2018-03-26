6fabe3828138cc4af176322ea9213ba3a870b6de
Ashi Krishnan <me@ashi.works>
2018-03-25 21:57:04 -0400

    Comments.

--- game/ai.js
+++ game/ai.js
@@ -1,14 +1,28 @@
 import reducer, {move, bad} from '.'
 
+/**
+ * moves(State) -> [...Action]
+ * 
+ * Return an array of actions which are valid moves from the given state.
+ */
 export const moves = game => [] // TODO
 
+/**
+ * score(game: State, move: Action) -> Number
+ * 
+ * Given a game state and an action, return a score for how good
+ * a move the action would be for the player whose turn it is.
+ * 
+ * Scores will be numbers from -1 to +1. 1 is a winning state, -1
+ * is state from which we can only lose.
+ */
 const score = (game, move) => {
   // TODO
 }
 
 /**
- * Return the best action for the current player.
+ * play(state: State) -> Action
  * 
- * @param {} state 
+ * Return the best action for the current player.
  */
 export default state => undefined // TODO
